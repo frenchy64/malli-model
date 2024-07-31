912e92c9f44c5d31a15af546ff3d399ef3bf3b32
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-07-31 13:58:22 -0500

    wip

---
+++ src/malli_model/trampoline_ref.clj
@@ -0,0 +1,34 @@
+(ns malli-model.trampoline-ref
+  (:refer-clojure :exclude [compile])
+  (:require [malli-model.trace :refer [trace-ns]]))
+(defn lookup [k R] (or (R k) (throw (ex-info (str "not in scope: " k) {}))))
+(defn found [k seen] (or (seen k) (throw (ex-info (str "not in scope: " k) {}))))
+(defn run [s f] (f))
+(defn compile' [s {:keys [R seen] :as o}]
+  (if (contains? R s) (compile' (lookup s R) o)
+      (case (if (vector? s) (first s))
+        := (let [[_ y] s] (fn [x k] (run s #(k (= x y)))))
+        :seqable (let [[_ c] s
+                       f (compile' c o)]
+                   (fn [x k]
+                     (run s
+                          (fn []
+                            (if (seqable? x)
+                              (letfn [(check [x k]
+                                        (if (empty? x)
+                                          (k true)
+                                          (fn []
+                                            (f (first x)
+                                               (fn [res]
+                                                 (fn []
+                                                   (if res
+                                                     (run s #(check (rest x) k))
+                                                     false)))))))]
+                                (check (seq x) k))
+                              (k false))))))
+        :ref (let [[_ c] s, f (delay (compile' c o))]
+               (fn [x k] (run s (fn [] (@f x k)))))
+        (throw (ex-info (str "invalid schema " (pr-str s)) {})))))
+(defn ^:no-trace compile [s o] (let [f (compile' s o)] (fn [x] (trampoline f x identity))))
+(defn validate [s o v] ((compile s o) v))
+(trace-ns)
---
+++ test/malli_model/trampoline_ref_test.clj
@@ -0,0 +1,27 @@
+(ns malli-model.trampoline-ref-test
+  (:require [clojure.test :refer :all]
+            [malli-model.trampoline-ref :as m]
+            [malli-model.trace :refer [trace-ns]]))
+
+(def nested-validator (delay (m/compile :nest {:R {:nest [:seqable [:ref :nest]]}})))
+
+(deftest validator-test
+  (is (m/validate [:= 1] {} 1))
+  (is (not (m/validate [:= 1] {} false)))
+  (is (not (m/validate [:seqable [:= 1]] {} 1)))
+  (is (m/validate [:seqable [:= 1]] {} [1]))
+  (is (not (m/validate [:seqable [:= 1]] {} 1)))
+  (is (m/validate :bar {:R {:bar [:= 1]}} 1))
+  (is (not (m/validate :bar {:R {:bar [:= 1]}} false)))
+  (is (not (m/validate :bar {:R {:bar [:= 1]}} false)))
+  (is (m/validate :bar {:R {:bar [:seqable [:ref :bar]]}} nil))
+  (is (m/validate :bar {:R {:bar [:seqable [:ref :bar]]}} [nil]))
+  (is (m/validate :bar {:R {:bar [:seqable [:ref :bar]]}} [[nil]]))
+  (is (m/validate :bar {:R {:bar [:seqable [:ref :bar]]}} [[[[[[nil]]]]]]))
+  (is (m/validate :bar {:R {:bar [:seqable [:ref :bar]]}} [[[[[[1]]]]]]))
+  (is (m/compile :nest {:R {:nest [:seqable [:ref :nest]]}}))
+  (is (@nested-validator [[nil]]))
+  (is (@nested-validator [[[nil]]]))
+  (is (@nested-validator [[[[nil]]]]))
+  (is (@nested-validator [[[[[nil]]]]]))
+  )
