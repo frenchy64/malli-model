8fa72ea96ab852569074dcadc4909b266bb94f44
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-07-31 15:27:48 -0500

    wip

--- test/malli_model/seen_test.clj
+++ test/malli_model/seen_test.clj
@@ -3,21 +3,17 @@
             [malli-model.seen :as m]
             [malli-model.trace :refer [trace-ns]]))
 
-(comment
-  (trace-ns malli-model.seen)
-  )
-
 (deftest validator-test
-  (is (m/validate 1 [:= 1] {}))
-  (is (not (m/validate false [:= 1] {})))
-  (is (not (m/validate 1 [:seqable [:= 1]] {})))
-  (is (m/validate [1] [:seqable [:= 1]] {}))
-  (is (not (m/validate 1 [:seqable [:= 1]] {})))
-  (is (m/validate :bar {:R {:bar [:= 1]}} 1))
-  (is (not (m/validate :bar {:R {:bar [:= 1]}} false)))
-  (is (not (m/validate :bar {:R {:bar [:= 1]}} false)))
-  (is (m/validate :bar {:R {:bar [:seqable :bar]}} nil))
-  (is (m/validate :bar {:R {:bar [:seqable :bar]}} [[[[[[nil]]]]]]))
-  (is (m/validate :bar {:R {:bar [:seqable :bar]}} [[[[[[1]]]]]] ))
-  (is (m/compile :nest {:R {:nest [:seqable :nest]}}))
+  (is (true? (m/validate [:= 1] {} 1)))
+  (is (false? (m/validate [:= 1] {} false)))
+  (is (false? (m/validate [:seqable [:= 1]] {} 1)))
+  (is (true? (m/validate [:seqable [:= 1]] {} [1])))
+  (is (false? (m/validate [:seqable [:= 1]] {} 1)))
+  (is (true? (m/validate :bar {:R {:bar [:= 1]}} 1)))
+  (is (false? (m/validate :bar {:R {:bar [:= 1]}} false)))
+  (is (false? (m/validate :bar {:R {:bar [:= 1]}} false)))
+  (is (true? (m/validate :bar {:R {:bar [:seqable :bar]}} nil)))
+  (is (true? (m/validate :bar {:R {:bar [:seqable :bar]}} [[[[[[nil]]]]]])))
+  (is (false? (m/validate :bar {:R {:bar [:seqable :bar]}} [[[[[[1]]]]]] )))
+  (is (fn? (m/compile :nest {:R {:nest [:seqable :nest]}})))
   )
